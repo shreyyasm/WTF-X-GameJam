d00d1c70260c5818bbcaa0000704b6c7534692b5
Shreyas Mourya <shreyasmourya5@gmail.com>
2024-07-20 00:08:58 +0530

    Camera System Done

--- Assets/Scripts/CameraAi.cs
+++ Assets/Scripts/CameraAi.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class CameraAi : MonoBehaviour
 {
+    public Transform pos1;
+    public Transform pos2;
+
+    // The target marker.
+    public Transform target;
+
+    // Angular speed in radians per sec.
+    public float speed = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = pos1;
     }
-
     // Update is called once per frame
     void Update()
     {
+       
+        if (transform.rotation.y > 0.5)
+        {
+            target = pos2;
+        }
+        if (transform.rotation.y < -0.5)
+        {
+            target = pos1;
+        }
+
+        Vector3 targetDirection = target.position - transform.position;
+        float singleStep = speed * Time.deltaTime;
+
+        // Rotate the forward vector towards the target direction by one step
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+
+        // Draw a ray pointing at our target in
+        Debug.DrawRay(transform.position, newDirection, Color.red);
+
+        transform.rotation = Quaternion.LookRotation(newDirection);
+
         
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("GameOver");
+        }
+    }
 }
+
