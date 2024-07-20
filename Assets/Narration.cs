0fa4c5431f791398628ae9e3bcb43cd8c922a820
Shreyas Mourya <shreyasmourya5@gmail.com>
2024-07-20 18:40:24 +0530

    Assets Added

---
+++ Assets/Narration.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Narration : MonoBehaviour
+{
+    public AudioClip Narration1;
+    public AudioClip Narration2;
+    public AudioClip Narration3;
+    public AudioClip Narration4;
+    public AudioClip Narration5;
+    public AudioClip Narration6;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
