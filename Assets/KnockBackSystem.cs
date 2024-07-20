0930ce2159b449b139b86f79887c3278bb750663
Shreyas Mourya <shreyasmourya5@gmail.com>
2024-07-20 12:42:21 +0530

    VFXs and SFXs System Added

---
+++ Assets/KnockBackSystem.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockBackSystem : MonoBehaviour
+{
+    public static KnockBackSystem Instance;
+    public GameObject MixingMachinePos;
+    public GameObject MainBeakerPos;
+    public GameObject SinkPos;
+    
+
+    //VFXs
+    public GameObject ChemicalPickupVFX;
+    public GameObject ChemicalDropVFX;
+    public GameObject WrongCompundVFX;
+    public GameObject RightCompundVFX;
+    public GameObject ChemicalRinseVFX;
+    public GameObject TestubeBurstVFX;
+    public GameObject AntidoteCreatedVFX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChemicalPickup(Transform Position)
+    {
+        Instantiate(ChemicalPickupVFX, Position.position, Quaternion.identity);
+        SoundManager.Instance.ChemicalPickup();
+    }
+    public void ChemicalDrop()
+    {
+        Instantiate(ChemicalDropVFX, MainBeakerPos.transform.position, Quaternion.identity);
+        SoundManager.Instance.ChemicalDrop();
+    }
+    public void WrongCompund()
+    {
+        Instantiate(WrongCompundVFX, MixingMachinePos.transform.position, Quaternion.identity);
+        SoundManager.Instance.WrongCompund();
+    }
+    public void RightCompund()
+    {
+        Instantiate(RightCompundVFX, MixingMachinePos.transform.position, Quaternion.identity);
+        SoundManager.Instance.WrongCompund();
+    }
+    public void ChemicalRinse()
+    {
+        Instantiate(ChemicalRinseVFX, SinkPos.transform.position, Quaternion.identity);
+        SoundManager.Instance.ChemicalRinse();
+    }
+    public void TestTubeBurst(Transform Position)
+    {
+        Instantiate(TestubeBurstVFX, Position.position, Quaternion.identity);
+        SoundManager.Instance.TestTubeBurst();
+    }
+    public void AntidoteCreated(Transform Position)
+    {
+        Instantiate(AntidoteCreatedVFX, Position.position, Quaternion.identity);
+        SoundManager.Instance.AntidoteCreated();
+    }
+}
--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance;
-    [SerializeField] AudioClip punchSFX; 
-    [SerializeField] AudioClip keyPressSFX;
-    [SerializeField] AudioClip wrongKeySFX;
+
+    public AudioClip ChemicalPickupSFX;
+    public AudioClip ChemicalDropSFX;
+    public AudioClip WrongCompundSFX;
+    public AudioClip RightCompundSFX;
+    public AudioClip ChemicalRinseSFX;
+    public AudioClip TestubeBurstSFX;
+    public AudioClip AntidoteCreatedSFX;
+
+    public AudioClip machineTurnOnSFX;
     // [SerializeField] AudioClip punchSFX;
     //[SerializeField] AudioClip punchSFX;
 
@@ -24,16 +31,40 @@ public class SoundManager : MonoBehaviour
     {
 
     }
-    public void CallPunchSFX()
+    public void ChemicalPickup()
     {
-        AudioSource.PlayClipAtPoint(punchSFX, Camera.main.transform.position, 0.4f);
+        AudioSource.PlayClipAtPoint(ChemicalPickupSFX, Camera.main.transform.position, 0.4f);
+    }
+    public void ChemicalDrop()
+    {
+        AudioSource.PlayClipAtPoint(ChemicalDropSFX, Camera.main.transform.position, 0.4f);
+    }
+    public void WrongCompund()
+    {
+        AudioSource.PlayClipAtPoint(WrongCompundSFX, Camera.main.transform.position, 0.4f);
+    }
+    public void RightCompund()
+    {
+        AudioSource.PlayClipAtPoint(RightCompundSFX, Camera.main.transform.position, 0.4f);
+    }
+    public void ChemicalRinse()
+    {
+        AudioSource.PlayClipAtPoint(ChemicalRinseSFX, Camera.main.transform.position, 0.4f);
+    }
+    public void TestTubeBurst()
+    {
+        AudioSource.PlayClipAtPoint(TestubeBurstSFX, Camera.main.transform.position, 0.4f);
+    }
+    public void AntidoteCreated()
+    {
+        AudioSource.PlayClipAtPoint(AntidoteCreatedSFX, Camera.main.transform.position, 0.4f);
     }
     public void CallKeyPressSFX()
     {
-        AudioSource.PlayClipAtPoint(keyPressSFX, Camera.main.transform.position);
+        //AudioSource.PlayClipAtPoint(keyPressSFX, Camera.main.transform.position);
     }
     public void CallWrongKeySFX()
     {
-        AudioSource.PlayClipAtPoint(wrongKeySFX, Camera.main.transform.position);
+        //AudioSource.PlayClipAtPoint(wrongKeySFX, Camera.main.transform.position);
     }
 }
